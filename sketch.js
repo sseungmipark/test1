addc3885e8813ba919292a2afd89e98c536bf004
sseungmipark <141466562+sseungmipark@users.noreply.github.com>
2023-12-16 15:08:29 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ let stars=[];
 let factor = 100;
 let speedSlider;
 function setup() {
-  createCanvas(windowsWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
 
   //DOM. createSildr(최소, 최대, 기본값)
   speedSlider=createSlider(0,20,5);
@@ -36,10 +36,10 @@ function draw() {
   //속도가 빨라지면 원이 커짐=> 속도를 크기로 매칭
   //3D 공간감 작=>크게 맵핑함.
   let d =map(star.z, 0, 400, 10 , 1);
-  //circle(x, y, d);
+  circle(x, y, d);
   
-  textSize(d*5);
-  text("성찬",x,y);
+  //textSize(d*5);
+  //text("*",x,y);
   stroke(255);
   line(x,y,px,py);
   star.pz=star.z;
