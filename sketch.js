a75d6ec93aacecdbb640371742ea4d7d068822cf
sseungmipark <141466562+sseungmipark@users.noreply.github.com>
2023-12-16 15:07:17 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 //p5canvas용 width, height 설정(설정이 꼬이기 때문)
-let width=windowWidth, height=windowHeight;
+//let width=windowWidth, height=windowHeight;
 let stars=[];
 let factor = 100;
 let speedSlider;
